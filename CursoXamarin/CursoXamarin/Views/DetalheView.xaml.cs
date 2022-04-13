2fcddc6b7efc16483b7c847189d110b8aa783cf0
Matheus Bueno <bueno_matheus@outlook.com>
2022-04-13 03:33:00 -0300

    App Curso Xamarin

---
+++ CursoXamarin/CursoXamarin/Views/DetalheView.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CursoXamarin.Views
+{
+    //[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetalheView : ContentPage
+    {
+        private const int freio_abs = 800;
+        private const int ar_condicionado = 1000;
+        private const int mp3_player= 500;
+        public Veiculo Veiculo { get; set; }
+
+        public string TextoFreioABS
+        {
+            get
+            {
+                return string.Format("Freio ABS - R$ {0}", freio_abs);
+            }
+        }
+
+        public string TextoArCondicionado
+        {
+            get
+            {
+                return string.Format("Ar Condicionado - R$ {0}", ar_condicionado);
+            }
+        }
+
+        public string TextoMP3Player
+        {
+            get
+            {
+                return string.Format("MP3 Player - R$ {0}", mp3_player);
+            }
+        }
+
+        bool temFreioABS;
+        public bool TemFreioABS
+        {
+            get
+            {
+                return temFreioABS;
+            }
+            set
+            {
+                temFreioABS = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+
+        bool temArCondicionado;
+        public bool TemArCondicionado
+        {
+            get
+            {
+                return temArCondicionado;
+            }
+            set
+            {
+                temArCondicionado = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+
+        bool temMP3Player;
+        public bool TemMP3Player
+        {
+            get
+            {
+                return temMP3Player;
+            }
+            set
+            {
+                temMP3Player = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+
+        public string ValorTotal
+        {
+            get
+            {
+                return string.Format("Valor Total: R$ {0}", Veiculo.Preco
+                + (TemFreioABS ? freio_abs : 0)
+                + (TemArCondicionado ? ar_condicionado : 0)
+                + (TemMP3Player ? mp3_player : 0)
+
+                );
+            }
+        }
+
+        public DetalheView(Veiculo veiculo)
+        {
+            InitializeComponent();
+            this.Veiculo = veiculo;
+            this.BindingContext = this; 
+        }
+
+        private void buttonProximo_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new AgendamentoView(this.Veiculo));
+        }
+    }
+}
---
+++ CursoXamarin/CursoXamarin/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CursoXamarin.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CursoXamarin.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
